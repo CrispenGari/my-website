2a8e3f96c0a3dd1a196f1936f66b651c4e0ee236
crispengari <Crispengari@gmail.com>
2023-05-21 09:58:06 +0200

    ee

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,6 +1,6 @@
-const environment = "dev";
+const environment = "prod";
 
 export const baseUrl =
   environment === "dev"
     ? "http://localhost:3001"
-    : "https://crispengari-server.onrender.com";
+    : "https://crispengari.onrender.com";
